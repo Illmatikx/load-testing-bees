0f567fae6e0bee21dc0dfa43a8ab8a725852889f
Illmatikx <30461882+Illmatikx@users.noreply.github.com>
2020-03-25 20:11:59 +0300

    tuned RTMP publisher script (#1)
    * added flv example
    * added ffmpeg
    * fixed pkill with ffmpeg
    * ffmpeg to ENV VARS
    * +x on ffmpeg
    * PATH var moved to set_timeout function
    Co-authored-by: Alexander Skamarokha <alexander.skamarokha@oxagile.com>

--- rtmpbee-publisher/script/rtmpbee-publisher.sh
+++ rtmpbee-publisher/script/rtmpbee-publisher.sh
@@ -34,7 +34,7 @@ file=$6
 function shutdown {
   pid=$1
   file=$2
-  kill -9 "$pid" && rm -f "$file" && echo "PID(${pid}) stopped." || echo "Failure to kill ${pid}."
+  for pids in $(ps -ef | awk '/rtmp/ {print $2}'); do kill -9 $pids; done
   echo "Attack ended at $(date '+%d/%m/%Y %H:%M:%S')"
   return 0
 }
@@ -50,11 +50,12 @@ function set_timeout {
   t=$2
   f=$3
   isLast=$4
-  echo "Will kill ${id} in ${t} seconds..."
+  PATH=$PATH:$PWD
+  #echo "Will kill ${id} in ${t} seconds..."
   if [ $isLast -eq 1 ]; then
     (sleep "$t"; shutdown "$id" "$f" || echo "Failure to kill ${id}."; return 0)
   else
-    (sleep "$t"; kill -9 "$id" && rm -f "$f" && echo "PID(${id}) stopped." || echo "Failure to kill ${id}.")&
+    (sleep "$t";   for pids in $(ps -ef | awk '/rtmp/ {print $2}'); do kill -9 $pids; done || echo "Failure to kill ${id}.")&
   fi
   return 0
 }
@@ -69,7 +70,8 @@ for ((i=0;i<amount;i++)); do
   stream_file="${file}_${i}"
   cp "$file" "$stream_file"
   # </dev/null tells ffmpeg to not look for input
-  ffmpeg -re -stream_loop -1 -fflags +genpts -i "$stream_file" -c copy -f flv "$target" 2>/dev/null &
+  #ffmpeg -re -stream_loop -1 -fflags +genpts -i "$stream_file" -c copy -f flv "$target" 2>/dev/null &
+  ffmpeg -re -stream_loop -1 -fflags +genpts -i "$stream_file" -c copy -f flv "$target"&
   isLast=0
   if [ $i -eq $((amount - 1)) ]; then
     isLast=1
