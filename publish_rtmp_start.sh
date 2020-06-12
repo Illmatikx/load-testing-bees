95ceaecf70d857f5a731a1a776eb5822b0fe14b7
Alexander Skamarokha <alexander.skamarokha@oxagile.com>
2020-06-12 15:52:30 +0300

    added h264 and vp8 source files

--- publish_rtmp_start.sh
+++ publish_rtmp_start.sh
@@ -4,7 +4,9 @@
 #target endpoint
 endpoint='https://ceo-perf.oxagile.com/api/v1/video/streams'
 #example file for streaming
-stream_file='./media_files/example.flv'
+#stream_file='./media_files/vp6f_288p_25fps.flv'
+stream_file='./media_files/h264_1080p_30fps.mp4'
+#stream_file='./media_files/vp8_1080p_30fps.webm'
 #number of publishers
 publishers=10
 #test duration
