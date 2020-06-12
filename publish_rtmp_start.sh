54a07e5840fe76c68f77d53ad6429deaed95a20b
Alexander Skamarokha <alexander.skamarokha@oxagile.com>
2020-06-12 21:11:43 +0300

    changed default test parameters

--- publish_rtmp_start.sh
+++ publish_rtmp_start.sh
@@ -5,12 +5,13 @@
 endpoint='https://ceo-perf.oxagile.com/api/v1/video/streams'
 #example file for streaming
 #stream_file='./media_files/vp6f_288p_25fps.flv'
-stream_file='./media_files/h264_1080p_30fps.mp4'
+#stream_file='./media_files/h264_1080p_30fps.mp4'
 #stream_file='./media_files/vp8_1080p_30fps.webm'
+stream_file='./media_files/flv1_1080p_25fps.flv'
 #number of publishers
-publishers=10
+publishers=1
 #test duration
-duration=60
+duration=1200
 #pause between starting new publishers
 pause=30
 ###############################
