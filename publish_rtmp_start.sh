85e2b716a7a924f2a1022b614928d10a6f3dac6e
Alexander Skamarokha <alexander.skamarokha@oxagile.com>
2020-08-07 19:54:02 +0300

    60fps source added

--- publish_rtmp_start.sh
+++ publish_rtmp_start.sh
@@ -5,7 +5,8 @@
 endpoint='https://ceo-perf.oxagile.com/api/v1/video/streams'
 #example file for streaming
 #stream_file='home/admin/load-testing-bees/media_files/vp6f_288p_25fps.flv'
-stream_file='/home/admin/load-testing-bees/media_files/h264_1080p_30fps.mp4'
+#stream_file='/home/admin/load-testing-bees/media_files/h264_1080p_30fps.mp4'
+stream_file='/home/admin/load-testing-bees/media_files/h264_1080p_60fps.mp4'
 #stream_file='home/admin/load-testing-bees/media_files/vp8_1080p_30fps.webm'
 #stream_file='home/admin/load-testing-bees/media_files/flv1_1080p_25fps.flv'
 #number of publishers
